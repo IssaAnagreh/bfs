9c92550a50106c91b5d2ddb6c2caf9a9384464de
IssaAnagreh <isa_anagreh@hotmail.com>
2022-07-02 19:28:33 +0400

    don't add new todo if empty feild

--- src/screens/home/reducer.ts
+++ src/screens/home/reducer.ts
@@ -6,7 +6,11 @@ export const homeReducer = (state: HomeStateType, action: HomeActionType) => {
     case 'ITEMS':
       return {...state, items: action.payload};
     case 'ADD_ITEM':
-      return {...state, items: [action.payload, ...state.items]};
+      if (action.payload.title) {
+        return {...state, txt: '', items: [action.payload, ...state.items]};
+      } else {
+        return state;
+      }
     case 'CHANGE_TXT':
       return {...state, txt: action.payload};
     case 'REMOVE_ITEM':
